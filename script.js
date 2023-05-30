bbd0d84768024c56cc2f90433e748a23724077b2
Brevin Abraham <brevinabraham98@live.com>
2023-05-30 18:58:42 +0100

    updates to all files

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const splash = document.querySelector(".splashscreen");
+
+document.addEventListener('DOMContentLoaded',e => {
+    setTimeout(() => {
+        console.log(splash.classList)
+        splash.classList.add('displacenone');
+    }, 2000);
+})
+
+
 let todayDate = new Date();
 
 document.getElementById("showDate").innerHTML = todayDate;
